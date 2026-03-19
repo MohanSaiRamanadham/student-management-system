19f74031d4d0ee3319286dbe1db3cf045dc2a057
Mohan Sai Ramanadham <ramanadhammohansai@gmail.com>
2026-03-20 00:16:16 +0530

    unused imports removed

--- src/main/java/com/college/StudentManagementSystem/StudentManagementSystemApplication.java
+++ src/main/java/com/college/StudentManagementSystem/StudentManagementSystemApplication.java
@@ -1,11 +1,7 @@
 package com.college.StudentManagementSystem;
 
-import ch.qos.logback.core.net.SyslogOutputStream;
-import org.apache.tomcat.util.digester.SystemPropertySource;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.http.converter.json.GsonBuilderUtils;
-import org.w3c.dom.ls.LSOutput;
 
 @SpringBootApplication
 public class StudentManagementSystemApplication {
